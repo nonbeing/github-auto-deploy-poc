72c8e19998de3f69a832e4f7310ea831aadab624
nonbeing <anattaspace@gmail.com>
2019-06-18 16:46:13 +0530

    Update configs

--- deploy-tools/deploy.sh
+++ deploy-tools/deploy.sh
@@ -7,9 +7,20 @@ cd ${PROJECT_DIR}
 git pull
 
 # enable virtualenv (called 'discord-bot') for discord-bot
+source /home/auto-deployer/.local/bin/virtualenvwrapper.sh
 workon discord-bot
 
 # restart discord-bot
-pgrep -f discord-bot.py | xargs kill
-python discord-bot.py
+pid=$(pgrep -f discord-bot.py)
+echo "pid of discord bot to kill: ${pid}"
+
+
+if [ ! -z "${pid}" ];   # if there is a pid of an existing discord bot
+then
+    kill ${pid}
+fi
+
+python discord-bot.py &
+
+exit 0;
 
