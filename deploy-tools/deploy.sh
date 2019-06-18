2480773c262f4a0b6606cf6cffab1957e5404a44
nonbeing <anattaspace@gmail.com>
2019-06-18 16:10:10 +0530

    fix deploy.sh to user virtualenv

--- deploy-tools/deploy.sh
+++ deploy-tools/deploy.sh
@@ -6,12 +6,10 @@ PROJECT_DIR=/srv/projects/github-auto-deploy-poc
 cd ${PROJECT_DIR}
 git pull
 
+# enable virtualenv (called 'discord-bot') for discord-bot
+workon discord-bot
+
 # restart discord-bot
 pgrep -f discord-bot.py | xargs kill
 python discord-bot.py
-
-# pgrep -f GitAutoDeploy.py | xargs kill
-
-# cd ${AUTODEPLOY_DAEMON_DIR}
-# python GitAutoDeploy.py -d
 
